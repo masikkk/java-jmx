42b93ab7ce1c4a9fa4ea7fdb382125e38d4f14ea
masikkk <masi.1989@163.com>
2017-10-24 17:14:22 +0800

    完善标准MBean RMI连接器代理

--- jmx/jmx-server/src/main/java/com/masikkk/jmx/agent/StandardAgentRMI.java
+++ jmx/jmx-server/src/main/java/com/masikkk/jmx/agent/StandardAgentRMI.java
@@ -18,15 +18,22 @@ public class StandardAgentRMI {
         mBeanServer.registerMBean(new HelloStandard(), new ObjectName("MBeanTest:name=StandardMBean"));
 
         try {
+        	//创建远程对象注册表Registry的实例，被创建的Registry将在指定的端口（默认1099）上侦听到来的请求
             LocateRegistry.createRegistry(8899);
-            JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:8899/jmxrmi");
+            
+            String ip_port = "localhost" + ":" + "8899";
+            String jmxAddress = "service:jmx:rmi://" + ip_port + "/jndi/rmi://" + ip_port + "/jmxrmi";
+            //String jmxAddress = "service:jmx:rmi:///jndi/rmi://localhost:8899/jmxrmi";
+            JMXServiceURL url = new JMXServiceURL(jmxAddress); //JMX连接器服务器地址
+            
+            //创建位于给定地址的连接器服务器，并指定该连接器服务器连接到的MBean服务器
             JMXConnectorServer jcs = JMXConnectorServerFactory.newJMXConnectorServer(url, null, mBeanServer);
-            jcs.start();
+            
+            jcs.start(); //激活连接器服务器，即开始侦听客户端连接。
+            
             System.out.println("JMX连接服务器已启动！请使用JConsole连接远程进程localhost:8899进行测试！");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 	}
-
 }
