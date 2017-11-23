5502cad36a198bf1eb6c514dfa6a2494dc761aba
masikkk <masi.1989@163.com>
2017-11-23 17:01:12 +0800

    模型mbean加些注释

--- jmx/jmx-server/src/main/java/com/masikkk/jmx/mbean/model/CarModelMBeanUtils.java
+++ jmx/jmx-server/src/main/java/com/masikkk/jmx/mbean/model/CarModelMBeanUtils.java
@@ -27,27 +27,29 @@ public class CarModelMBeanUtils {
 	//创建Car类的模型MBean描述信息
 	private static ModelMBeanInfo createModelMBeanInfo() {
 		ModelMBeanInfo modelMBeanInfo = null;
-		ModelMBeanAttributeInfo[] attributeInfos = new ModelMBeanAttributeInfo[1]; //属性
-		ModelMBeanOperationInfo[] operationInfos = new ModelMBeanOperationInfo[4]; //方法
+		ModelMBeanAttributeInfo[] attributeInfos = new ModelMBeanAttributeInfo[1]; //属性数组
+		ModelMBeanOperationInfo[] operationInfos = new ModelMBeanOperationInfo[4]; //方法数组
 		try {
 			//属性color
 			attributeInfos[0] = new ModelMBeanAttributeInfo(
-					"color", 
-					"java.lang.String", 
-					"Car's color", 
-					true, 
-					true, 
-					false, 
-					null);
+					"color",  //属性名称
+					"java.lang.String", //属性的类型名称或类名称
+					"Car's color", //属性的可读描述
+					true, //isReadable 
+					true, //isWritable 
+					false, //isIs，属性是否有"is"方法 
+					null //Descriptor类型的描述符，如果为null则自动创建默认描述符
+					);
 			
 			//方法getColor()
 			operationInfos[0] = new ModelMBeanOperationInfo(
-					"getColor", 
-					"getColor Method", 
-					null, 
-					"java.lang.String", 
-					MBeanOperationInfo.INFO, 
-					null);
+					"getColor", //方法名称
+					"getColor Method", //操作的可读描述
+					null, //描述方法参数的MBeanParameterInfo对象数组
+					"java.lang.String", //方法返回值的类型
+					MBeanOperationInfo.INFO, //方法的影响，读取操作
+					null //Descriptor类型的描述符，如果为null则自动创建默认描述符
+					);
 		
 			//方法setColor()的参数
 			MBeanParameterInfo[] setColorParams = new MBeanParameterInfo[]{
@@ -60,7 +62,7 @@ public class CarModelMBeanUtils {
 					"setColor Method", 
 					setColorParams, 
 					"void", 
-					MBeanOperationInfo.ACTION, 
+					MBeanOperationInfo.ACTION, //方法的影响，写入操作
 					null);		
 			
 			//方法printColor()
@@ -69,7 +71,7 @@ public class CarModelMBeanUtils {
 					"printColor Method", 
 					null, 
 					"void", 
-					MBeanOperationInfo.INFO, 
+					MBeanOperationInfo.INFO, //方法的影响，读取操作
 					null);
 			
 			//方法printColor(String,String)的参数
@@ -84,18 +86,19 @@ public class CarModelMBeanUtils {
 					"printColor(String,String) Method", 
 					printColorParams, 
 					"void", 
-					MBeanOperationInfo.ACTION, 
+					MBeanOperationInfo.ACTION, //方法的影响，写入操作
 					null);			
 			
 			//构造ModelMBeanInfo结构
 			modelMBeanInfo = new ModelMBeanInfoSupport(
-					"com.masikkk.jmx.mbean.model.Car", 
-					null, 
-					attributeInfos, 
-					null, 
-					operationInfos, 
-					null, 
-					null);
+					"com.masikkk.jmx.mbean.model.Car", //MBean 的类名称
+					"Car类的模型MBean描述", //ModelMBean 的可读描述
+					attributeInfos, //描述属性的ModelMBeanAttributeInfo对象数组
+					null, //描述构造函数的ModelMBeanConstructorInfo 对象数组
+					operationInfos, //描述方法的ModelMBeanOperationInfo 对象数组
+					null, //描述通知的ModelMBeanNotificationInfo 对象数组
+					null //Descriptor类型的描述符，如果为null则自动创建默认描述符
+					);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}	
